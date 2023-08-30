3a29f4b5fd3ee5923f20f1a275c1f06d5f58f0d4
sertifisering <yonggi_jeong@outlook.com>
2023-08-30 13:53:18 +0200

    Input Output

--- PGR107/Lecture/20230828.py
+++ PGR107/Lecture/20230828.py
@@ -1,6 +1,8 @@
+# case 1
 # name = input("Type your name: ")
 # print("\n You are name is : " + str(name))\
 
+# case 2
 # print("name: " + str(input("name : "))
 #     + "\n age: " + str(input("age: "))
 #     + "\n phone: " + str(input("phone: "))     
@@ -10,13 +12,17 @@ dataSet = {
     "label":["name", "age","phone","address"]
 }
 
+# case 3
 # for label in dataSet["label"]:
 #     print(label+ " is a" + str(input(label + ":")))
 
+# case 4
 def getUserInfo(dataSet):
   resultSet = []
   for label in dataSet["label"]:
-      resultSet.append(label + " is a  " + str(input(label + ": ")))
+      resultSet.append(label + " is a  " + str(input(label + ": " )))
   return resultSet
 
 print(getUserInfo(dataSet))
+
+#case 5
